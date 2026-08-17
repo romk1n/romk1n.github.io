7273690181b69006d582335aee7b37ca0b835a41
romk1n <roman@techneed.ev>
2026-08-17 14:59:16 +0100

    new structure to fix github pages

--- assets/static/dh8bjtGtoxdiB7LHcVFwv/_buildManifest.js
+++ assets/static/dh8bjtGtoxdiB7LHcVFwv/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/./_next/:path+",
-        "destination": "/_next/:path+"
+        "source": "/./assets/:path+",
+        "destination": "/assets/:path+"
       }
     ],
     "fallback": []
