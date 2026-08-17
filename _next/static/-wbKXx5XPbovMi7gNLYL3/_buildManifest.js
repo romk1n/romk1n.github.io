9b4a3cfddecbeb549215143d7472b868f2a90bb9
romk1n <roman@techneed.ev>
2026-08-17 14:53:50 +0100

    fixing ai workflow

--- _next/static/-wbKXx5XPbovMi7gNLYL3/_buildManifest.js
+++ _next/static/-wbKXx5XPbovMi7gNLYL3/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/./_next/:path+",
+        "destination": "/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
